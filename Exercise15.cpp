2d683f28a4d60abe97b6c11b28d175ac7e76d9a6
LeAnhTuan20205136 <Tuan.LA205136@sis.hust.edu.vn>
2021-03-28 17:00:55 +0700

    Exercise 1.5

---
+++ Exercise15.cpp
@@ -0,0 +1,12 @@
+/* Suppose that you have been given a
+$100 music store gift voucher. Write
+an algorithm for buying some CDs
+with the voucher.*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    
+}
