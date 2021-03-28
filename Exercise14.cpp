c0634f1e8824a1d645815d050b9ad6d1d9d20375
LeAnhTuan20205136 <Tuan.LA205136@sis.hust.edu.vn>
2021-03-28 17:39:12 +0700

    update Ex14

--- Exercise14.cpp
+++ Exercise14.cpp
@@ -13,5 +13,34 @@ weather conditions. */
 using namespace std;
 
 int main() {
-    
+    string name;
+    int thoitiet, hoatdong;
+    cout << "Nhap ten cua ban: " << endl;
+    cin >> name;
+    cout << "Chao " << name << "! Hay chon 1 trong 2 dang thoi tiet 1. hot va 2. snow : " << endl;
+    cin >> thoitiet;
+    cout << "Hay chon 1 trong 2 hoat dong 1. going to the beach va 2. skiing : " << endl;
+    cin >> hoatdong;
+    switch (thoitiet) {
+        
+        case 1 : {
+            if (hoatdong == 1) {
+                cout << "Hay mac do boi"; 
+            }
+            else cout << "Khong co tuyet";
+            break;
+        }
+       
+        case 2 : {
+            if (hoatdong == 1){
+                cout << "Ban khong nen di boi";
+            }
+            else cout << "mac do am va mang van truot";
+            break;
+        }
+        
+        default : {
+            cout << "Khong hop le, vui long nhap lai!";
+        }
+    }
 }
