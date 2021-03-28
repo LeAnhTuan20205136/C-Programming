8d9e82787396fb7a58569e2396f3d703c40d80af
LeAnhTuan20205136 <81509460+LeAnhTuan20205136@users.noreply.github.com>
2021-03-28 23:07:12 +0700

    Update Exercise15.cpp

--- Exercise15.cpp
+++ Exercise15.cpp
@@ -10,7 +10,7 @@ using namespace std;
 int main() {
     
     int voucher = 100;
-    int CD, sl1, th1 = 0, sl2, th2 = 0, sl3, th3 = 0;
+    int CD = 1, sl1, th1 = 0, sl2, th2 = 0, sl3, th3 = 0;
     
     cout << "Cua hang CD: " << endl << "1. CD ve Idols (10$)" << endl;
     cout << "2. CD ve tieng Nhat (15$)" << endl << "3. CD ve C Programming (25$)" << endl;
@@ -23,51 +23,51 @@ int main() {
         case 0 : {
             cout << "Cam on quy khach!!" << endl;
             break;
-        }
+            }
         
         case 1 : {
             cout << "Nhap so luong muon mua: " << endl;
             cin >> sl1;
-            th1 = th1 + sl1;
             if((sl1 * 10) > voucher){
                 cout << "Vuot qua so tien hien co, khong thuc hien duoc giao dich!" << endl;
             }
             else {
+                th1 = th1 + sl1;
                 voucher = voucher - (sl1 * 10);
                 cout << "So tien hien tai con : " << voucher << endl;
             break;
-        }
+            }
         }
          case 2 : {
             cout << "Nhap so luong muon mua: " << endl;
             cin >> sl2;
-            th2 = th2 + sl2;
             if((sl2 * 15) > voucher){
                 cout << "Vuot qua so tien hien co, khong thuc hien duoc giao dich!" << endl;
             }
             else {
+                th2 = th2 + sl2;
                 voucher = voucher - (sl2 * 15);
                 cout << "So tien hien tai con : " << voucher << endl;
             break;
-        }
+            }
         }
         case 3 : {
             cout << "Nhap so luong muon mua: " << endl;
             cin >> sl3;
-            th3 = th3 + sl3;
             if((sl3 * 25) > voucher){
                 cout << "Vuot qua so tien hien co, khong thuc hien duoc giao dich!" << endl;
             }
             else {
+                th3 = th3 + sl3;
                 voucher = voucher - (sl3 * 25);
                 cout << "So tien hien tai con : " << voucher << endl;
             break;
-        }
+            }
         }
         default : {
             cout << "Khong hop le, vui long nhap lai!" << endl;
             break;
-        }
+            }
         }
     }
     
