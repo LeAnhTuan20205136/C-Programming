98776dc15f0643e4da6be47e826cd673a7b2874e
LeAnhTuan20205136 <Tuan.LA205136@sis.hust.edu.vn>
2021-03-28 16:55:47 +0700

    Exercise 1.4

---
+++ Exercise14.cpp
@@ -0,0 +1,17 @@
+/* Write an algorithm for choosing clothes
+to wear for different activities involving
+different weather conditions (e.g., going
+to the beach when it is hot, skiing in the
+snow, etc.). The algorithm should make
+it clear how the choice of clothes
+depends on the weather and the activity,
+and should be able to cope with a range of
+weather conditions. */
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    
+}
