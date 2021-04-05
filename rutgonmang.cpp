5bb7919224b03dc6aadeb6f08d2a1fb6fa34cbb1
LeAnhTuan20205136 <81509460+LeAnhTuan20205136@users.noreply.github.com>
2021-04-05 10:57:50 +0700

    Update rutgonmang.cpp

--- rutgonmang.cpp
+++ rutgonmang.cpp
@@ -2,24 +2,30 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
 	int n, a[1000];
 	bool kt = true;
 	cout << "nhap so phan tu: " << endl;
 	cin >> n;
 	cout << "nhap day so input: " << endl;
-	for (int i = 0; i < n; i++){
+	for (int i = 0; i < n; i++)
+	{
 		cin >> a[i];
 	}
 	cout << "Output la:" << a[0] << " ";
-	for (int i = 1 ; i < n; i++){
-		for(int j = 0; j < i; j++){
-			if(a[j] == a[i]) {
+	for (int i = 1 ; i < n; i++)
+	{
+		for(int j = 0; j < i; j++)
+		{
+			if(a[j] == a[i]) 
+			{
 				kt = false;
 				break;
 			}			
 		}
-		if (kt == true){
+		if (kt == true)
+		{
 			cout << a[i] << " ";
 		}					
 	}
